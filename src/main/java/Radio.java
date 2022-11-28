4fe55ad4ea92fb9abe4444aeb2e7219238d25847
QA-Lexx <ilin_001@mail.ru>
2022-11-28 21:57:05 +0300

    Change codes and create new tests in files Radio and RadioTest

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,63 +1,62 @@
 public class Radio {
-    public int currentVolume;
 
-    public int getCurrentVolume() {
-        return currentVolume;
+    private int currentStation;
+    private int currentVolume;
+
+    public void next() {
+        if (currentStation < 9) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
     }
 
-    public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume < 0) {
-            return;
+    public void prev() {
+        if (currentStation > 0) {
+            currentStation--;
+        } else {
+            currentStation = 9;
         }
-        if (newCurrentVolume > 10) {
-            return;
-        }
-        currentVolume = newCurrentVolume;
     }
 
-    public void IncreaseVolume() {
+    public void increaseVolume() {
         if (currentVolume < 10) {
-            currentVolume = currentVolume + 1;
+            currentVolume++;
         }
     }
 
-    public void setToMaxVolume() {
-        currentVolume = 10;
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
     }
-
-    public void setToMinVolume() {
-        currentVolume = 0;
-    }
-
-    public int currentStation;
 
     public int getCurrentStation() {
         return currentStation;
     }
 
-    public void setCurrentStation(int newCurrentStation) {
-        if (newCurrentStation < 0) {
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
             return;
         }
-        if (newCurrentStation > 9) {
+        if (currentStation > 9) {
             return;
         }
-        currentStation = newCurrentStation;
+        this.currentStation = currentStation;
     }
 
-    public void SwitchStation() {
-        if (currentStation < 9) {
-            currentStation = currentStation + 1;
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
         }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
     }
 
-    public void increaseStationNext() {
-        int target = currentStation + 1;
-        currentStation = target;
-    }
-
-    public void reduceStationPrevious() {
-        int target = currentStation - 1;
-        currentStation = target;
-    }
 }
